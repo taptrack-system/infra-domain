1440c48fb1dd00839192861bd6f21fe62bba2261
JulianeMaran32 <julianemaran@gmail.com>
2025-09-27 20:36:18 -0300

    [gateway-service] Add Cache Caffeine

---
+++ config-service/src/main/java/com/infradomain/configservice/service/PropertyService.java
@@ -0,0 +1,12 @@
+package com.infradomain.configservice.service;
+
+/**
+ * config-service
+ * @author Juliane Maran
+ * @since 27/09/2025
+ */
+public class PropertyService {
+
+    // TODO: Implementar funcionalidades da classe PropertyService
+
+}
---
+++ discovery-service/src/main/java/com/infradomain/discoveryservice/service/ServiceRegistry.java
@@ -0,0 +1,12 @@
+package com.infradomain.discoveryservice.service;
+
+/**
+ * discovery-service
+ * @author Juliane Maran
+ * @since 27/09/2025
+ */
+public class ServiceRegistry {
+
+    // TODO: Implementar funcionalidades da classe ServiceRegistry
+
+}
