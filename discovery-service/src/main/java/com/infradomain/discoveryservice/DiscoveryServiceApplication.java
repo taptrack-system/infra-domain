2e379e8764708c3aa7698779e1b6ac696d097a68
JulianeMaran32 <julianemaran@gmail.com>
2025-09-27 20:36:43 -0300

    [discovery-service] Add Cache Caffeine

--- discovery-service/src/main/java/com/infradomain/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/infradomain/discoveryservice/DiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.infradomain.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableCaching
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
