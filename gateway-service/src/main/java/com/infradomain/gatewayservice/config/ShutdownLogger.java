dbde924ffb58ca577f4cee202fb5284f13474840
JulianeMaran32 <julianemaran@gmail.com>
2025-09-27 23:12:00 -0300

    atualização dos arquivos pom, application e logger

---
+++ gateway-service/src/main/java/com/infradomain/gatewayservice/config/ShutdownLogger.java
@@ -0,0 +1,12 @@
+package com.infradomain.gatewayservice.config;
+
+/**
+ * gateway-service
+ * @author Juliane Maran
+ * @since 27/09/2025
+ */
+public class ShutdownLogger {
+
+    // TODO: Implementar funcionalidades da classe ShutdownLogger
+
+}
