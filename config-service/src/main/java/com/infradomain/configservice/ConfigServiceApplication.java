f5d11a6f32f79c90168b3d432f42cd577bc94702
JulianeMaran32 <julianemaran@gmail.com>
2025-09-27 21:58:56 -0300

    [config-service] Add Cache Caffeine

--- config-service/src/main/java/com/infradomain/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/infradomain/configservice/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.infradomain.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableCaching
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
