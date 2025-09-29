66bac720ef0e31298e914d3a1c0695d8e05ea6e8
JulianeMaran32 <julianemaran@gmail.com>
2025-09-29 14:15:59 -0300

    chore: atualiza os arquivos application, docker compose, pom e logger

--- config-service/src/main/java/com/infradomain/configservice/config/ShutdownLogger.java
+++ config-service/src/main/java/com/infradomain/configservice/config/ShutdownLogger.java
@@ -1,12 +1,24 @@
 package com.infradomain.configservice.config;
 
+import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * config-service
+ *
  * @author Juliane Maran
  * @since 27/09/2025
  */
+@Configuration
 public class ShutdownLogger {
 
-    // TODO: Implementar funcionalidades da classe ShutdownLogger
+  private static final Logger log = LoggerFactory.getLogger(ShutdownLogger.class);
+
+  @PreDestroy
+  public void onShutdown() {
+    log.info("🛑 {} shutting down gracefully...", getClass().getSimpleName());
+  }
 
 }
