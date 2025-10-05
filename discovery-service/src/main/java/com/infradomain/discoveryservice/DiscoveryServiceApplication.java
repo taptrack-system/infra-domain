1c939433d90a45fa9cc9cb49ecb7dd5fbe249cce
JuhMaran <julianemaran@gmail.com>
2025-10-05 17:33:49 -0300

    refactor: atualiza Discovery Service

--- discovery-service/src/main/java/com/infradomain/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/infradomain/discoveryservice/DiscoveryServiceApplication.java
@@ -2,16 +2,14 @@ package com.infradomain.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableCaching
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DiscoveryServiceApplication.class, args);
+  }
 
 }
