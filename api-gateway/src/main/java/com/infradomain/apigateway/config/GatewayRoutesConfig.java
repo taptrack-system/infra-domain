8258374544229fd269c0a5894808debb3e727b10
JuhMaran <julianemaran@gmail.com>
2025-10-08 21:07:56 -0300

    chore[api-gateway]: adiciona configurações

---
+++ api-gateway/src/main/java/com/infradomain/apigateway/config/GatewayRoutesConfig.java
@@ -0,0 +1,10 @@
+package com.infradomain.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+  // TODO: CONFIGURAÇÃO DAS ROTAS
+
+}
