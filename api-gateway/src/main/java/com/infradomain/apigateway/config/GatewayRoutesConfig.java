a780af8bfbd040afb40f74c32d1bd17b7f879958
JuhMaran <julianemaran@gmail.com>
2025-10-09 21:00:08 -0300

    API Gateway e Eureka Server integrados com Config Server

--- api-gateway/src/main/java/com/infradomain/apigateway/config/GatewayRoutesConfig.java
+++ api-gateway/src/main/java/com/infradomain/apigateway/config/GatewayRoutesConfig.java
@@ -1,10 +1,22 @@
 package com.infradomain.apigateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayRoutesConfig {
 
-  // TODO: CONFIGURAÇÃO DAS ROTAS
+  @Bean
+  public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    return builder.routes()
+      .route("identity-profiles", r -> r
+        .path("/identity-profiles/**")
+        .filters(f -> f.stripPrefix(1)
+          .dedupeResponseHeader("Access-Control-Allow-Origin", "RETAIN_FIRST"))
+        .uri("lb://identity-profiles"))
+      .build();
+  }
 
 }
